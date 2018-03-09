d9b4982c20e745d8218efc5c1deb602e7725f671
Anthony <anthonyjlower@gmail.com>
2018-03-09 00:18:31 -0600

    Hid port and created dotenv file

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const auctionServer = require('./socket-server');
 
 
 
-const server = app.listen(4000, () => {
-	console.log('server is listening on port 4000')
+const server = app.listen(process.env.PORT, () => {
+	console.log('server is listening on port: ' + process.env.PORT)
 })
 
 auctionServer(server)
